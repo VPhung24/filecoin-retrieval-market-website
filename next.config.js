404ba89ad55aaefb67859196967888e7b86ee3a1
Vivian Phung <dev+github@vivianphung.com>
2024-10-01 14:43:39 -0400

    Update webpack

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
-const { WebpackBundleSizeAnalyzerPlugin } = require('webpack-bundle-size-analyzer');
-
 /**
  * Export next configuration.
  */
@@ -13,17 +6,5 @@ module.exports = {
   compiler: {
     styledComponents: true
   },
-  trailingSlash: true,
-  webpack: (config, { webpack }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['svg-inline-loader?idPrefix&classPrefix']
-    });
-
-    config.plugins.push(
-      new WebpackBundleSizeAnalyzerPlugin('stats.txt')
-    );
-
-    return config;
-  }
+  trailingSlash: true
 };
